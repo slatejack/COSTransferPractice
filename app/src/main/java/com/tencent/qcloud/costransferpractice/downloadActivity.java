aeb7c0403037660916eeff4012d3878bbdbcb014
Slate <1815846446@qq.com>
2019-05-31 09:54:38 +0800

    Initial commit

---
+++ app/src/main/java/com/tencent/qcloud/costransferpractice/downloadActivity.java
@@ -0,0 +1,13 @@
+package com.tencent.qcloud.costransferpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class downloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+
+    }
+}
